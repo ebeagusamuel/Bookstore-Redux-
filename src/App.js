948d50089aeb0e7a1c2fde259cf6d1bc8311df62
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-10-30 15:01:02 +0200

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import BookList from './BookList';
-import BookForm from './BookForm';
+import BookList from './containers/BookList';
+import BookForm from './containers/BookForm';
 
 function App() {
   return (
-    <div>
+    <div className="my-4 d-flex flex-column align-items-center justify-content-center">
       <BookForm />
       <BookList />
     </div>
---
+++ src/containers/BookForm.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const BookForm = () => <div>BookForm</div>;
+export default BookForm;
